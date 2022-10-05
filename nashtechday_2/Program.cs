9c7a53baa86034b40b1398e5ca717858bfa5308e
nguyenthan88 <nguyenvanthanvp18@gmail.com>
2022-10-06 01:53:07 +0700

    complete

--- nashtechday_2/Program.cs
+++ nashtechday_2/Program.cs
@@ -6,12 +6,12 @@
         {
             var members = new List<Member>()
             {
-                new Member("nguyen","van anh","Male", new DateTime(2002,09,22),"0987654","Ha Nam",true),
+                new Member("Nguyen","Van Anh","Male", new DateTime(2002,09,22),"0987654","Ha Nam",true),
                 new Member("Tran","Van Nga","Female", new DateTime(2001,05,22),"0987654","Ha Noi",false),
                 new Member("Do","Thai Tuan","Male", new DateTime(1998,05,22),"0987654","Ha Noi",true),
                 new Member("Vu","Hoang Nam","Male", new DateTime(2002,09,22),"0987654","Vinh Phuc",true),
-                new Member("Hoang","van anh","Male", new DateTime(2000,09,12),"0987654","Ha Noi",false),
-                new Member("Kim","Lan","Female", new DateTime(1989,05,22),"0987654","HCM",true),
+                new Member("Hoang","Van Anh","Male", new DateTime(2000,09,12),"0987654","Ha Noi",false),
+                new Member("Kim","Lan","Female", new DateTime(1989,05,22),"0987654","HCM City",true),
                 new Member("Dang","The Mau","Male", new DateTime(2002,09,22),"0987654","Hai Phong",false),
                 new Member("Tran","Anh","Female", new DateTime(1989,09,22),"0987654","Cao Bang",false),
             };
@@ -22,70 +22,73 @@
                 Console.Write("------------------------------------------------");
                 Console.Write("\n\n");
                 Console.Write("\nThis is the choice:\n");
-                Console.Write("1 - Return a list of members who is Male " +
+                Console.Write("1 - Return a list of members who is Male. " +
                               "\n2 - Return the oldest one based on “Age”." +
                               "\n3 - Return a new list that contains Full Name only." +
-                              "\n4 - Return 3 lists:" +
-                              "\n5 - Return the first person who was born in Ha Noi" +
+                              "\n4 - Return 3 lists." +
+                              "\n5 - Return the first person who was born in Ha Noi." +
                               "\n6 - Exit.\n");
                 Console.Write("\nEnter your choice: ");
 
-                int opt;
-                opt = Convert.ToInt32(Console.ReadLine());
+                int opt = Convert.ToInt32(Console.ReadLine());
 
                 switch (opt)
                 {
                     case 1:
-                        Console.Write("Return a list of members who is Male\n");
+                        Console.Write("Return a list of members who is Male.\n");
                         Console.Write("------------------------------------\n");
-                        var membersMale = from member in members
+
+                        var maleMembers = from member in members
                                           where member.Gender == "Male"
                                           select member;
 
-                        foreach (var member in membersMale)
+                        foreach (var member in maleMembers)
                         {
                             Console.WriteLine(member.Information);
                         }
                         break;
                     case 2:
-                        Console.Write("Return the oldest one based on “Age”\n");
+                        Console.Write("Return the oldest one based on “Age”.\n");
                         Console.Write("------------------------------------\n");
 
-                        var oldMember = members.OrderByDescending(x => x.Age).FirstOrDefault();
+                        var oldestMember = members.OrderByDescending(x => x.Age).FirstOrDefault();
 
-                        if (oldMember != null)
+                        if (oldestMember != null)
                         {
-                            Console.WriteLine(oldMember.Information);
+                            Console.WriteLine(oldestMember.Information);
                         }
                         break;
                     case 3:
-                        Console.Write("Return a new list that contains Full Name only\n");
+                        Console.Write("Return a new list that contains Full Name only.\n");
                         Console.Write("----------------------------------------------\n");
 
-                        var fullName = from member in members
-                                       select member;
+                        var fullNames = from member in members
+                                        select new
+                                        {
+                                            FirstName = member.FirstName,
+                                            LastName = member.LastName
+                                        };
 
-                        foreach (var member in fullName)
+                        foreach (var member in fullNames)
                         {
                             Console.WriteLine($" {member.FirstName,10} {member.LastName,20}");
                         }
                         break;
                     case 4:
-                        Console.Write("Return 3 list\n");
-                        Console.Write("1 - List of members who has birth year is 2000 " +
+                        Console.Write("Return 3 list:\n");
+                        Console.Write("1 - List of members who has birth year is 2000. " +
                                 "\n2 - List of members who has birth year greater than 2000." +
-                                "\n3 - List of members who has birth year less than 2000" +
+                                "\n3 - List of members who has birth year less than 2000." +
                                 "\n4 - Exit.\n");
                         Console.Write("--------------------------------------------------------\n");
                         Console.Write("\nEnter your choice: ");
 
-                        int opt1;
-                        opt1 = Convert.ToInt32(Console.ReadLine());
+                        int choice = Convert.ToInt32(Console.ReadLine());
 
-                        switch (opt1)
+                        switch (choice)
                         {
                             case 1:
-                                Console.Write("\n List of members who has birth year is 2000\n");
+                                Console.Write("\nList of members who has birth year is 2000.\n");
 
                                 var member2000 = from member in members
                                                  where member.DateOfBirth.Year == 2000
@@ -97,7 +100,7 @@
                                 }
                                 break;
                             case 2:
-                                Console.Write("\n List of members who has birth year greater than 2000\n");
+                                Console.Write("\nList of members who has birth year greater than 2000.\n");
 
                                 var memberGreater2000 = from member in members
                                                         where member.DateOfBirth.Year > 2000
@@ -109,7 +112,7 @@
                                 }
                                 break;
                             case 3:
-                                Console.Write("\nList of members who has birth year less than 2000\n");
+                                Console.Write("\nList of members who has birth year less than 2000.\n");
 
                                 var memberLess2000 = from member in members
                                                      where member.DateOfBirth.Year < 2000
@@ -122,24 +125,24 @@
                                 break;
 
                             default:
-                                Console.Write("Enter a valid option\n");
+                                Console.Write("Enter a valid option:\n");
                                 break;
                         }
                         break;
                     case 5:
-                        Console.Write("Return the first person who was born in Ha Noi\n");
+                        Console.Write("Return the first person who was born in Ha Noi.\n");
                         Console.Write("----------------------------------------------\n");
 
-                        var firstMember = members.Where(m => m.BirthPlace == "Ha Noi").FirstOrDefault();
+                        var haNoiMember = members.FirstOrDefault(m => m.BirthPlace == "Ha Noi");
 
-                        if (firstMember != null)
+                        if (haNoiMember != null)
                         {
-                            Console.WriteLine(firstMember.Information);
+                            Console.WriteLine(haNoiMember.Information);
                         }
                         break;
 
                     default:
-                        Console.Write("Enter a valid option\n");
+                        Console.Write("Enter a valid option:\n");
                         break;
                 }
             }
