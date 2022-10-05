f42ccb24fe58ab34341beaf49e2f9bbf313064e3
nguyenthan88 <nguyenvanthanvp18@gmail.com>
2022-10-06 01:41:35 +0700

    complete day3

---
+++ nashtechday_3/Program.cs
@@ -0,0 +1,22 @@
+﻿class ClockApplication
+{
+    public class Clock
+    {
+        public delegate void ClockApp();
+        public event ClockApp OnChange = delegate { };
+        public void ClockDisplay()
+        {
+            OnChange();
+        }
+    }
+    static void Main(String[] args)
+    {
+        while (true)
+        {
+            Thread.Sleep(1000);
+            Clock clock = new Clock();
+            clock.OnChange += () => Console.WriteLine(DateTime.Now);
+            clock.ClockDisplay();
+        }
+    }
+}
