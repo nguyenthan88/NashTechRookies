b138a3d87ede51c16b1dcd6d72e8be2bae1b5c6c
nguyenthan88 <nguyenvanthanvp18@gmail.com>
2022-10-06 01:43:15 +0700

    complete day3

---
+++ nashtechday_3_2/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void IsPrimeNumber(int number)
+    {
+        bool Prime = true;
+        Console.WriteLine("Prime Numbers between 0 to {0} : ", number);
+        for (int a = 2; a <= number; a++)
+        {
+            for (int b = 2; b <= number; b++)
+            {
+                if (a != b && a % b == 0)
+                {
+                    Prime = false;
+                    break;
+                }
+            }
+            if (Prime)
+            {
+                Console.Write(($"{a,3}"));
+            }
+            Prime = true;
+        }
+    }
+    static async Task Task1()
+    {
+        await Task.Run(() =>
+           {
+               Console.Write("enter integer:");
+               int number = Convert.ToInt32(Console.ReadLine());
+               IsPrimeNumber(number);
+           });
+    }
+    static async Task Main(string[] args)
+    {
+        await Task1();
+        Console.Write("\n");
+        Console.WriteLine("-------------------");
+        Console.ReadKey();
+    }
+}
